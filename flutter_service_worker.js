1e500751870c89909c0975611daa3d61be7df629
henrikk <henrik.kother@study.hs-duesseldorf.de>
2021-05-10 18:14:05 +0200

    dadwa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "cb9a5b33c574d364975779f894c34754",
-"index.html": "4c3c362942ff7700ac033283df89b4e8",
-"/": "4c3c362942ff7700ac033283df89b4e8",
-"main.dart.js": "5ef26a2e0c1b0ad9916d5f099082de65",
+"index.html": "18a48af6c62059d5435291ce32a6d2c4",
+"/": "18a48af6c62059d5435291ce32a6d2c4",
+"main.dart.js": "6814281a1fc4578342de9e9c2db295f6",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
